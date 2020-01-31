84258c4fb372299eef2c679800a8e410233bc13e
Finn Glas <finnmglas@gmail.com>
2020-01-31 21:57:36 +0100

    Make it (un-) installable, Closes #1

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/sh
+
+# run this script to install the Json Class database
+# by running 'jcdb' from the terminal, you can access the
+
+cp jcdb.py /usr/bin/jcdb
+
+echo "Installed 'jcdb' application to '/usr/bin',"
+echo "you can now reach it from the terminal"
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/sh
+
+# run this script to uninstall the Json Class database
+
+rm /usr/bin/jcdb
+
+echo "Uninstalled the 'jcdb' application"
