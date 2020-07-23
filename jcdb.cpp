1521a2ee423164167c989c3b09a9c187ea99c476
Finn M Glas <finn@finnmglas.com>
2020-07-23 23:34:24 +0200

    Improve argument parsing
    Parse command, database and data instance. Ready for the implementation 
    of further jcdb commands.

--- jcdb.cpp
+++ jcdb.cpp
@@ -14,19 +14,26 @@
 
 /* Imports */
 #include <ctype.h>
+#include <cstring>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <iostream>
 
 /* Defines */
 #define DB_LOCATION "/var/jcdb"
 #define DB_VERSION "v2.0.0"
 
 #define USAGE "usage: jcdb\
- [-v] [-h]"
+ [-v] [-h] <command> [<db>] [<class>] [<instance>]"
 
 #define HELP "Common JCDB commands:\n\
 \n\
+'help': print this text\n\
+    help\n\
+\n\
+'version': print the JCDB version\n\
+    version\
 "
 
 /* The application */
@@ -34,54 +41,47 @@ int main (int argc, char **argv) {
   int parse_char;
   int parse_non_arg_i;
 
-  char *cvalue = NULL;
+  char *parsed_command = NULL;
+  char *parsed_db = NULL;
+  char *parsed_instance = NULL;
 
-  /* Parse arguments */
-  {
-    /* No arguments -> Usage */
-    if (argc == 0) {
-      printf("%s\n", USAGE);
+  bool parsed_flag_help = false;
+  bool parsed_flag_version = false;
+
+  parse_arguments: {
+    /* No arguments -> Help */
+    if (argc == 1) {
+      printf("%s\n\n%s\n", USAGE, HELP);
+      return 0;
     }
 
     opterr = 0; // return '?' rather than throwning errors
 
     /* Loop through options args */
-    while ((parse_char = getopt(argc, argv, "vhc:")) != -1) {
+    while ((parse_char = getopt(argc, argv, "hv")) != -1) {
       switch (parse_char) {
-
-        /* -v | Print the JCDB version */
-        case 'v':
-          printf("JCDB %s\n", DB_VERSION);
-          return 0;
-          break;
 
         /* -h | Print some helpful commands */
         case 'h':
-          printf("%s\n\n%s\n", USAGE, HELP);
-          return 0;
+          parsed_flag_help = true;
           break;
 
-        case 'c':
-          cvalue = optarg;
+        /* -v | Print the JCDB version */
+        case 'v':
+          parsed_flag_version = true;
           break;
 
         /* Something went wrong */
         case '?':
-          /* Additional info required -> Usage */
-          if (optopt == 'c')
-            fprintf (stderr,
-              "Expected an argument for -%c.\n\n%s\n",
-              optopt, USAGE);
-
           /* Unknown option -> Usage */
-          else if (isprint (optopt))
-            fprintf (stderr,
-              "Unknown option `-%c'.\n\n%s\n",
+          if (isprint(optopt))
+            fprintf(stderr,
+              "Unknown option '-%c'.\n\n%s\n",
               optopt, USAGE);
 
           /* Unknown character -> Usage */
           else
-            fprintf (stderr,
+            fprintf(stderr,
               "Unknown option character `\\x%x'.\n\n%s\n",
               optopt, USAGE);
 
@@ -96,11 +96,37 @@ int main (int argc, char **argv) {
     /* Non- option args */
     for (parse_non_arg_i = optind;
         parse_non_arg_i < argc; parse_non_arg_i++) {
-      printf ("Non-option argument %s\n", argv[parse_non_arg_i]);
+      if (parse_non_arg_i == 1)
+        parsed_command = argv[parse_non_arg_i];
+      else if (parse_non_arg_i == 2)
+        parsed_db = argv[parse_non_arg_i];
+      else if (parse_non_arg_i == 3)
+        parsed_instance = argv[parse_non_arg_i];
     }
   }
 
-  /* TODO: execute the given command */
+  execute_command: {
+
+    /* help | Print some helpful commands (same as -h) */
+    if (parsed_flag_help ||
+        parsed_command && strcmp(parsed_command, "help") == 0) {
+      printf("%s\n\n%s\n", USAGE, HELP);
+    }
+
+    /* version | Print the JCDB version (same as -v) */
+    else if (parsed_flag_version ||
+        strcmp(parsed_command, "version") == 0) {
+      printf("JCDB %s\n", DB_VERSION);
+    }
+
+    /* Unknown command */
+    else {
+      fprintf (stderr,
+        "Unknown command '%s'.\n\n%s\n",
+        parsed_command, USAGE);
+      return 3;
+    }
+  }
 
   return 0;
 }
