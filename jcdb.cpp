6e51d8c72ff70a12ad4d0988374257e31fd1587b
Finn M Glas <finn@finnmglas.com>
2020-07-23 22:26:38 +0200

    Handle cases for displaying errors and usage
    Usage will be shown almost any time the user gets something wrong or 
    unrecogniced by jcdb

--- jcdb.cpp
+++ jcdb.cpp
@@ -22,7 +22,7 @@
 #define DB_LOCATION "/var/jcdb"
 #define DB_VERSION "v2.0.0"
 
-#define USAGE "jcdb\
+#define USAGE "usage: jcdb\
  [-v] [-h]"
 
 #define HELP "Common JCDB commands:\n\
@@ -31,55 +31,76 @@
 
 /* The application */
 int main (int argc, char **argv) {
-  int aflag = 0;
-  int bflag = 0;
+  int parse_char;
+  int parse_non_arg_i;
+
   char *cvalue = NULL;
-  int index;
-  int c;
 
   /* Parse arguments */
   {
-  opterr = 0; // return '?' rather than throwning errors
+    /* No arguments -> Usage */
+    if (argc == 0) {
+      printf("%s\n", USAGE);
+    }
 
-  while ((c = getopt (argc, argv, "vhc:")) != -1) {
-    switch (c) {
+    opterr = 0; // return '?' rather than throwning errors
 
-      /* -v | Print the JCDB version */
-      case 'v':
-        printf("JCDB %s\n", DB_VERSION);
-        return 0;
-        break;
+    /* Loop through options args */
+    while ((parse_char = getopt(argc, argv, "vhc:")) != -1) {
+      switch (parse_char) {
 
-      /* -h | Print some helpful commands */
-      case 'h':
-        printf("usage: %s\n\n%s\n", USAGE, HELP);
-        return 0;
-        break;
+        /* -v | Print the JCDB version */
+        case 'v':
+          printf("JCDB %s\n", DB_VERSION);
+          return 0;
+          break;
 
-      case 'c':
-        cvalue = optarg;
-        break;
-      case '?':
-        if (optopt == 'c')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return 1;
-      default:
-        abort();
+        /* -h | Print some helpful commands */
+        case 'h':
+          printf("%s\n\n%s\n", USAGE, HELP);
+          return 0;
+          break;
+
+        case 'c':
+          cvalue = optarg;
+          break;
+
+        /* Something went wrong */
+        case '?':
+          /* Additional info required -> Usage */
+          if (optopt == 'c')
+            fprintf (stderr,
+              "Expected an argument for -%c.\n\n%s\n",
+              optopt, USAGE);
+
+          /* Unknown option -> Usage */
+          else if (isprint (optopt))
+            fprintf (stderr,
+              "Unknown option `-%c'.\n\n%s\n",
+              optopt, USAGE);
+
+          /* Unknown character -> Usage */
+          else
+            fprintf (stderr,
+              "Unknown option character `\\x%x'.\n\n%s\n",
+              optopt, USAGE);
+
+          return 1; // exit with error 1
+
+        /* Something worse went wrong */
+        default:
+          return 2;
       }
+    }
+
+    /* Non- option args */
+    for (parse_non_arg_i = optind;
+        parse_non_arg_i < argc; parse_non_arg_i++) {
+      printf ("Non-option argument %s\n", argv[parse_non_arg_i]);
     }
   }
 
   /* TODO: execute the given command */
 
-  printf ("cvalue = %s\n", cvalue);
-
-  for (index = optind; index < argc; index++)
-    printf ("Non-option argument %s\n", argv[index]);
   return 0;
 }
